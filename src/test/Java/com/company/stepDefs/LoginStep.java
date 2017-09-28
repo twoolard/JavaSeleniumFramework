bfd07ad2af9c700a20383d318d9078573371821f
twoolard <terry.woolard@optum.com>
2017-09-28 11:58:02 -0500

    more stuff

--- src/test/Java/com/company/stepDefs/LoginStep.java
+++ src/test/Java/com/company/stepDefs/LoginStep.java
@@ -16,49 +16,18 @@ public class LoginStep extends BaseUtil {
         this.base = base;
     }
 
-//    public class User
-//    {
-//        public String username;
-//        public String password;
-//
-//        public User(String userName, String Password) {
-//            username = userName;
-//            password = Password;
-//        }
-//    }
-
     @Given("^I navigate to the login page$")
     public void iNavigateToTheLoginPage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         page = LoginPage.visitPage(base.driver);
     }
 
-//    @And("^I enter the following for Login$")
-//    public void i_enter_the_following_for_Login(DataTable table) throws Throwable {
-////        List<List<String>> data = table.raw();
-////        String username = data.get(0).get(1).toString();
-////        String Password = data.get(1).get(1).toString();
-////
-////        System.out.println("The Value for username is: " + username);
-////        System.out.println("The Value for password is: " + Password);
-//
-//        List<User> users = new ArrayList<User>();
-//        users = table.asList(User.class);
-//
-//        for (User user: users){
-//            page.Login(user.username, user.password);
-//        }
-//    }
-
     @And("^I click login button$")
     public void iClickLoginButton() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         page.ClickLogin();
     }
 
     @Then("^I should see the userform page$")
     public void iShouldSeeTheUserformPage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         page.isDisplayed();
     }
 
